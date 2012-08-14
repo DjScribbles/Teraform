6b0737b3fe4e1a1be35a834857e0f59085dfba54
Joe S <joeshaw321@gmail.com>
2012-08-13 22:37:49 -0400

    Added some unfinished and unused classes
    Item is intended to be a unifying base class for items, and should allow
    for transitioning a block from an inventory item, being placed in a
    grid, being removed from the grid into the world, and picked up from the
    world into the players inventory.
    
    Belt is the hotbar for items.

---
+++ Teraform/Teraform/Belt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teraform
+{
+    public class Belt
+    {
+
+        int _selectedBeltIndex;
+        int _numberOfBeltItems;
+        int _numberOfQuickSlots;
+        //TODO belt item array
+        //TODO quick slot array
+
+        Belt(int beltSize, int quickSlots)
+        {
+            _numberOfBeltItems = beltSize;
+            _numberOfQuickSlots = quickSlots;
+            _selectedBeltIndex = 0;
+
+        }
+
+        public void SelectNextItem()
+        {
+            int starting_index = _selectedBeltIndex;
+            do
+            {
+                _selectedBeltIndex++;
+
+                if (_selectedBeltIndex >= _numberOfBeltItems)
+                    _selectedBeltIndex = 0;
+
+            } while (false && (_selectedBeltIndex != starting_index));
+        }
+
+        public void SelectPreviousItem()
+        {
+            int starting_index = _selectedBeltIndex;
+            do
+            {
+                if (_selectedBeltIndex <= 0)
+                    _selectedBeltIndex = _numberOfBeltItems;
+
+                _selectedBeltIndex--;
+            } while (false && (_selectedBeltIndex != starting_index));
+        }
+
+
+    }
+}
---
+++ Teraform/Teraform/Item.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Teraform
+{
+    class Item
+    {
+        public enum ITEM_STATE 
+        {
+            IN_WORLD,
+            IN_GRID,
+            IN_INVENTORY,
+            EQUIPPED
+        };
+        
+        private ITEM_STATE _currentState;
+        public Point _drawLocation;
+        public decimal _drawRotation;
+
+
+        public bool Use(Point location, GameCharacter user) 
+        {
+            switch (_currentState)
+            {
+                case ITEM_STATE.IN_GRID:
+                    break;
+
+                case ITEM_STATE.IN_INVENTORY:
+                    break;
+
+            }
+            return false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            switch (_currentState)
+            {
+                case ITEM_STATE.IN_GRID:
+
+                    break;
+
+                case ITEM_STATE.IN_INVENTORY:
+                    break;
+
+                case ITEM_STATE.EQUIPPED:
+                case ITEM_STATE.IN_WORLD:
+                    break;
+            }
+        }
+
+        public ITEM_STATE ItemState
+        {
+            get { return _currentState; }
+            set { _currentState = value; }
+        }
+
+        protected virtual bool UseFromInventory(Point target, GameCharacter user)
+        {
+            return false;
+        }
+
+        protected virtual bool UseFromGrid(GameCharacter user)
+        {
+            return false;
+        }
+    }
+}
